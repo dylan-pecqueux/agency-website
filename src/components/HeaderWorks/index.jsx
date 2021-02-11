607bde49b278d4bba64bd433f2f228337fb5e1ac
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2021-02-11 12:38:47 +0100

    add better button for the theme change

--- src/components/HeaderWorks/index.jsx
+++ src/components/HeaderWorks/index.jsx
@@ -4,11 +4,12 @@ import ListContext from 'ListContext';
 
 const HeaderWorks = () => {
   const listContext = useContext(ListContext);
+  const txtBtn = listContext.list ? '||| Change to card' : '- Change to list';
   return (
     <div className="works">
       <h1>Au fil des années, nous avons pu accompagner les meilleurs.</h1>
       <p>Découvrez pas à pas comment nous avons été présent pour lancer vos marques préférées.</p>
-      <button className="btn-work" type="button" onClick={listContext.change}>{listContext.list ? '- list' : '|| card'}</button>
+      <button className="btn-work" type="button" onClick={listContext.change}>{txtBtn}</button>
     </div>
   );
 };
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,10 +2,12 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import ThemeContext from 'ThemeContext';
+import lune from 'images/lune.png';
+import sun from 'images/sun2.png';
 
 const Navbar = () => {
   const theme = useContext(ThemeContext);
-  const textBtn = theme.theme === 'light' ? 'change to dark theme' : 'change to light theme';
+  const btnState = theme.theme === 'light' ? 'btn-left' : 'btn-right';
 
   return (
     <header>
@@ -16,7 +18,11 @@ const Navbar = () => {
           <Link to="/about">About</Link>
         </div>
         <div className="nav-theme">
-          <button className="btn-nav" type="button" onClick={theme.change}>{textBtn}</button>
+          <button className={`btn-theme ${btnState}`} type="button" onClick={theme.change}>
+            <div />
+            <img className="lune" src={lune} alt="icone lune" />
+            <img className="sun" src={sun} alt="icone lune" />
+          </button>
         </div>
       </nav>
     </header>
